3c573c486bf7edbcfbc07a291af371817757ad52
нр <xojayevfayzulla7@gmail.com>
2023-12-26 20:50:41 +0500

    boshqo yer ao'tdik

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+ alert("salom")
 
 // const array ={
 //     age: 24,
@@ -31,9 +31,37 @@
 
 // console.log(rang); 
 
-const follower = prompt("qaysi ", "xojaniiyozov")
+// const follower = prompt("qaysi ", "xojaniiyozov")
 
-console.log("https://instagram.com/"+follower);
+// console.log("https://instagram.com/"+follower);
 
-console.log(`https://instagram.com/${follower}`);
+// console.log(`https://instagram.com/${follower}`);
+
+"use strict"
+const numberOFSerias = prompt("nechta serial ko'rdingiz? "," 3");
+
+const seriasDB={
+    count:numberOFSerias,
+    series:{},
+    actors:{},
+    genres:[],
+    privat: false
+}
+
+const savol1= prompt("ohigi marta qaysi serailmni koridngiz?"," dsfdedg"),
+ baho1= prompt("nechi","5"),
+ savol2= prompt("ohigi marta qaysi serailmni koridngiz?"," dsfdedg"),
+baho2= prompt("nechi","4");
+
+// seriasDB.series={
+//     savol1:baho1,
+//     savol2:baho2
+// }
+
+
+
+seriasDB.series[savol1]=baho1;
+seriasDB.series[savol2]=baho2; 
+
+console.log(seriasDB);
 
