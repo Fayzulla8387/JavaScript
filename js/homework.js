1f152fb6dd040f592379c321b7373646257172dc
нр <xojayevfayzulla7@gmail.com>
2023-12-26 20:50:53 +0500

    boshqo yer ao'tdik

---
+++ js/homework.js
@@ -0,0 +1,158 @@
+"use strict"
+// let numberOFSerias = promt("nechta serial ko'rdingiz? "," 3");
+
+// const seriasDB={
+//     "count":numberOFSerias,
+//     "series":{},
+//     "actors":{},
+//     "genres":[],
+//     "privat": false
+// }
+
+// let savol1= prompt("ohigi marta qaysi serailmni koridngiz?"," dsfdedg");
+// let baho1= prompt("nechi","5");
+// let savol2= prompt("ohigi marta qaysi serailmni koridngiz?"," dsfdedg");
+// let baho2= prompt("nechi","4");
+
+// seriasDB.series={
+//     savol1:baho1,
+//     savol2:baho2
+// }
+
+// console.log(seriasDB);
+
+
+// var a="Fayzulla"
+
+// console.log(a[a.length-8]);
+
+// function what(nomi,yoshi){
+//    let result="";
+//    result+="nomi "+nomi+"yoshi "+yoshi;
+
+//    console.log(result);
+// }
+
+// what("itim", "5");
+
+// let myarray=[[4,5], [5,6] ,[ [1,2], 3,4]];
+// console.log(myarray[2][0][0]);
+
+// myarray=myarray.shift();
+// console.log(myarray);
+
+
+// let k="salom";
+
+// function s(){
+//    let k="vaaleykum";
+
+// return k;
+// }
+
+// console.log(s());
+
+// console.log(k);
+
+// function arr()
+// var array=[1,2,3];
+// console.log("before"+ JSON.stringify(array));
+
+// function t(val){
+//    if(val<13){
+//     console.log("13 dan kichik");
+//    }
+
+//    if(val<30){
+//     console.log("30 dan kichik");
+//    }
+
+
+//    console.log ("30 dan katta");
+// }
+// t(10);
+
+// let mydog={
+//     "name": "kuchuk",
+//     yoshi:3
+// }
+
+// console.log(mydog);
+
+
+// mydog["ovozi"]="woow";
+// console.log(mydog);
+
+// if(mydog.hasOwnProperty("name")){
+//     console.log(mydog["name"]);
+// }
+
+
+// var collection= {
+//     "2534":{
+//         "album" :"afad",
+//         "artist": "cdvsv",
+//         "tracks": [
+//             "dfafa",
+//             "adwefe"
+//         ]
+//     },
+
+//     "3245":{
+//         "album" :"afaddefew",
+//             "tacks": [
+//             "dfafa",
+//             "adwefe"
+//         ]
+//     },
+
+//     "1245":{
+//           "artist":"asfaf",
+//           "tracks":[]
+//     }
+// }
+
+
+// var collectionCopy=JSON.parse(JSON.stringify(collection));
+// // function update(id,prop,value){
+
+// // }
+
+// console.log(collectionCopy);
+
+// const obj= [
+//     {
+//        fname:"sara",
+//        lname:"conor",
+//        likes: ["dog","banana"]
+//     },
+//     {
+//         fname:"fayzulla",
+//         lname:"xojayev",
+//         likes: ["cat","pineapple"]
+//     }
+// ]
+
+// function ol(name, prop){
+//    for(let i=0;i<obj.length;i++){
+//     if(obj[i].fname==name || name==" " ){
+//          console.log(obj[i][prop]);
+//     }else{
+//         console.log("NOt found!!!");
+//     }
+//    }
+
+
+    
+// }
+
+// var element = ol(" ", "likes");
+
+
+
+// console.log(Math.floor(Math.random()*20));
+
+// console.log(parseInt("011", 2));
+
+console.log(5==3 ? true : false)
+
