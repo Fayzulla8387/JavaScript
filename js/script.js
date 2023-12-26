9e8513ddf066591a309b08102af430ce20a20c91
нр <xojayevfayzulla7@gmail.com>
2023-12-26 08:08:31 +0500

    qo'shildi

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// const number=10;
+
+// let Name="fayzulla";
+
+// const array ={
+//     age: 24,
+//     jacket: "black",
+//     isLong : false
+
+// }
+
+
+// console.log(array.age);
+
+
+// const place= confirm("Are you men?");
+
+// if(place==true){
+//     console.log("haha");
+// }
+
+
+// const currentPlace = prompt("Qayerdasan" ," Xorazm");
+
+// const age = prompt("H o a y?");
+// console.log(typeof(age));
+
+// const rang = [];
+
+// rang[0]=prompt("1-rang "," ");
+// rang[1]=prompt("2-rang " ,"red")
+
+// console.log(rang); 
+
+const follower = prompt("qaysi ", "xojaniiyozov")
+
+console.log("https://instagram.com/"+follower);
+
+console.log(`https://instagram.com/${follower}`);
